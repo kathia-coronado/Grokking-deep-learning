07e243bfe8e7a50c34002f882b6254f4929aa7fb
kathia Coronado <coronadokathia53@gmail.com>
2021-12-31 17:44:30 -0500

    Ch4 example problem

--- ch4_1.py
+++ ch4_1.py
@@ -1,7 +1,31 @@
-knob_weight = 0.5
-input = 0.5
-goal_pred = 0.8
-pred = input * knob_weight
-error = (pred - goal_pred)
-print (error)
-print(error ** 2)
+#Hot and cold learning demonstration 
+
+weight = 0.1 
+lr = 0.01 
+def neural_network(input, weight):
+    prediction = input * weight
+    return prediction
+
+number_of_toes = [8.5]
+win_or_loose_binary = [1] # won!! 
+input = number_of_toes[0]
+true = win_or_loose_binary[0]
+lr = 0.01
+
+pred = neural_network(input, weight)
+error = (pred - true) ** 2
+
+p_up = neural_network(input, weight + lr)
+e_up = (p_up - true) ** 2
+
+p_dn = neural_network(input, weight - lr)
+e_dn = (p_dn - true) ** 2
+
+if (error > e_dn or error > e_up):
+    if (e_dn < e_up):
+        weight -= lr
+    
+    if (e_up < e_dn):
+        weight += lr
+print(error, e_up, e_dn)
+print(weight)
